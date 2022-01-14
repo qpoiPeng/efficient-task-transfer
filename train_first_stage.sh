ce739314854074e7ee0098039c768e2308c429bd
hank0316 <b08902126@ntu.edu.tw>
2022-01-14 23:51:00 +0800

    fix training shell script

--- train_first_stage.sh
+++ train_first_stage.sh
@@ -1,8 +1,11 @@
-tasks=("stsb", "rte", "mrpc")
-for i in “${array[@]}”; do
-    python ./training_scripts/run_transfer.py \
+#!/bin/sh
+
+tasks=("stsb" "rte" "mrpc")
+for i in "${tasks[@]}"; do
+  echo $i
+  python ./training_scripts/run_transfer.py \
         --id 1 \
         --learning_rate 1e-4 \
         --num_train_epochs 10 \
-        i
+        $i
 done
